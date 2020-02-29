4a97922ea5d126aa5b221d103b27ff04148e35fc
Adam Wirehed <adam.wirehed@gmail.com>
2020-02-29 11:10:10 +0100

    Added code for all the tasks in assignment 3. Next step is to check it against the theory

--- StatInference/StatInfA3.py
+++ StatInference/StatInfA3.py
@@ -30,10 +30,12 @@ print("Confidence interval (95%, normal): {}\n".format(I_normal))
 totSize = len(dfMale) + len(dfFemale)
 s2Pooled = (len(dfMale) - 1)/(totSize - 2)*s2Male + (len(dfFemale) - 1)/(totSize - 2)*s2Female
 degF = totSize - 2
-t = 1.984   # Two sided t-test (95% confidence interval)
-I_ttest = [xyBar - t*m.sqrt(s2Pooled)*(totSize/(len(dfMale)*len(dfFemale))), xyBar + t*m.sqrt(s2Pooled)*(totSize/(len(dfMale)*len(dfFemale)))]
+t_obs = xyBar/m.sqrt(s2Pooled) * m.sqrt(totSize/(len(dfMale) * len(dfFemale)))
 
-print("Confidence interval (95%, t-test): {}\n".format(I_ttest))
+print("t-value: {}".format(t_obs))
+print("Df: {}".format(degF))
+print("p-value: {} = Not significant \n".format(stats.t.pdf(t_obs, degF)))
+
 
 # task iii, Rank sum test
 dfRanked = df
@@ -41,22 +43,86 @@ dfRanked = dfRanked.sort_values('temperature')
 
 sumMale = 0
 sumFemale = 0
+rank = 0
 
 for index, row in dfRanked.iterrows():
     if row["gender"] == 1:
-        sumMale += (index + 1)
+        sumMale += (rank + 1)
     elif row["gender"] == 2:
-        sumFemale += (index + 1)
+        sumFemale += (rank + 1)
     else:
         print("Data at index {} is weird \n".format(index))
+    rank += 1
 
-print(sumMale)
-print(sumFemale)
-print(sumMale + sumFemale)
-print((len(dfMale) + len(dfFemale)) * (len(dfMale) + len(dfFemale) + 1)/2)
+print("Rank sum male: {}".format(sumMale))
+print("Rank sum female: {}".format(sumFemale))
+print("Fraction male sample vs female sample: {}".format(len(dfMale)/len(dfFemale)))
+print("Normalized rank sum male: {}".format(sumMale * 1/(sumMale + sumFemale) * 20*(20+1)/2))
+print("Normalized rank sum female: {}".format(sumFemale * 1/(sumMale + sumFemale) * 20*(20+1)/2))
+print("Rank sum for males and females is not lower or higher than their bounds, Tl = 79, Tu = 131. Accept H0\n")
+
+# print(sumMale)
+# print(sumFemale)
+# print(sumMale + sumFemale)
+# print((len(dfMale) + len(dfFemale)) * (len(dfMale) + len(dfFemale) + 1)/2)
+
+
+# task b, i
+
+xyBarRate = dfMale["rate"].mean() - dfFemale["rate"].mean()
+
+s2MaleRate = dfMale["rate"].var()
+s2FemaleRate = dfFemale["rate"].var()
+
+SE_xyRate = m.sqrt(s2MaleRate/len(dfMale) + s2FemaleRate/len(dfFemale))
+I_normalRate = [xyBarRate - 1.96*SE_xyRate, xyBarRate + 1.96*SE_xyRate]
+
+print("Confidence interval (95%, normal): {}\n".format(I_normalRate))
+
+
+# b,ii - parametric test, two sample t-test
+
+totSize = len(dfMale) + len(dfFemale)
+s2PooledRate = (len(dfMale) - 1)/(totSize - 2)*s2MaleRate + (len(dfFemale) - 1)/(totSize - 2)*s2FemaleRate
+degF = totSize - 2
+t_obsRate = xyBarRate/m.sqrt(s2PooledRate) * m.sqrt(totSize/(len(dfMale) * len(dfFemale)))
+
+print("t-value: {}".format(t_obs))
+print("Df: {}".format(degF))
+print("p-value: {} = Not significant \n".format(stats.t.pdf(t_obsRate, degF)))
+
+dfRankedRate = df
+dfRankedRate = dfRanked.sort_values('rate')
+
+sumMaleRate = 0
+sumFemaleRate = 0
+rank = 1
+
+for index, row in dfRankedRate.iterrows():
+    if row["gender"] == 1:
+        sumMaleRate += (rank + 1)
+    elif row["gender"] == 2:
+        sumFemaleRate += (rank + 1)
+    else:
+        print("Data at index {} is weird \n".format(index))
+    rank += 1
+
+print("Rank sum male: {}".format(sumMaleRate))
+print("Rank sum female: {}".format(sumFemaleRate))
+print("Fraction male sample vs female sample: {}".format(len(dfMale)/len(dfFemale)))
+print("Normalized rank sum male: {}".format(sumMaleRate * 1/(sumMaleRate + sumFemaleRate) * 20*(20+1)/2))
+print("Normalized rank sum female: {}".format(sumFemaleRate * 1/(sumMaleRate + sumFemaleRate) * 20*(20+1)/2))
+print("Rank sum for males and females is not lower or higher than their bounds, Tl = 79, Tu = 131. Accept H0\n")
+
 
 sns.set()
 
-# measurements = dfMale["temperature"]
-# stats.probplot(measurements, dist="norm", plot=pylab)
-# pylab.show()
+measurements = dfMale["temperature"]
+stats.probplot(measurements, dist="norm", plot=pylab)
+plt.title("QQ-plot for temperature")
+pylab.show()
+
+measurements = dfMale["rate"]
+stats.probplot(measurements, dist="norm", plot=pylab)
+plt.title("QQ-plot for heart rate")
+pylab.show()
