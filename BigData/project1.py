0258479914d5720681535cca2db114a896f9d182
AdamWirehed <adam.wirehed@gmail.com>
2020-03-31 18:25:59 +0200

    Before I "rör om i grytan"

--- BigData/project1.py
+++ BigData/project1.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import sklearn.linear_model as kit
 import sklearn.neighbors as kNN
-from sklearn.model_selection import train_test_split as tts
+from sklearn.model_selection import KFold
 import sklearn.model_selection as mod
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -11,38 +11,48 @@ import seaborn as sns
 pathData = "/Users/adamwirehed/Documents/GitHub/Rdatasets/csv"
 
 
-## Depression dataset
+## Depression dataset split
 dfDep = pd.read_csv(pathData + "/carData/Ginzberg.csv")
-dfDepTrain, dfDepTest = tts(dfDep[['simplicity', 'depression']], test_size=0.2)
-xDep = dfDepTrain['simplicity'].values
-xDep = xDep.reshape(len(dfDepTrain), 1)
-yDep = dfDepTrain['depression'].values
-yDep = yDep.reshape(len(dfDepTrain), 1)
+n_splits = 5
+kf = KFold(n_splits=n_splits, shuffle=True)
+ix_train, ix_test = next(kf.split(dfDep), None)
+dfTrain = dfDep.iloc[ix_train]
+dfTest = dfDep.iloc[ix_test]
+xTrain = dfTrain['simplicity'].values
+xTrain = xTrain.reshape(len(dfTrain), 1)
+yTrain = dfTrain['depression'].values
+yTrain = yTrain.reshape(len(dfTrain), 1)
+xTest = (dfTest['simplicity'].values).reshape(len(dfTest), 1)
+yTest = (dfTest['depression'].values).reshape(len(dfTest), 1)
 
 # Linear regression
 lin_regDep = kit.LinearRegression()
-lin_regDep.fit(xDep, yDep)
+lin_regDep.fit(xTrain, yTrain)
 lin_test = kit.LinearRegression()
 
-#k-neighbors
+# k-neighbors
 k = 8
 kNN_regDep = kNN.KNeighborsRegressor(n_neighbors=k)
-kNN_regDep.fit(xDep, yDep)
+kNN_regDep.fit(xTrain, yTrain)
 kNN_test = kNN.KNeighborsRegressor(n_neighbors=k)
 
-xTest = (dfDepTest['simplicity'].values).reshape(len(dfDepTest), 1)
-yTest = (dfDepTest['depression'].values).reshape(len(dfDepTest), 1)
+# Cross-validation
+TE_Dep_lin = sum(yTest - lin_regDep.predict(xTest))
+TE_Dep_kNN = sum(yTest - kNN_regDep.predict(xTest))
 
-cv = mod.ShuffleSplit(n_splits=5, test_size=0.2, random_state=0)
-lin_cval = mod.cross_validate(lin_test, xDep, yDep, cv=cv)
-kNN_cval = mod.cross_validate(kNN_test, xDep, yDep, cv=cv)
-print(lin_cval['test_score'].mean())
-print(kNN_cval['test_score'].mean())
+print("Test error linear: {}".format(TE_Dep_lin))
+print("Test error kNN: {}".format(TE_Dep_kNN))
+
+""" cv = mod.ShuffleSplit(n_splits=1, test_size=0.2)
+lin_reg = mod.cross_validate(lin_test, xDep, yDep, cv=cv)
+kNN_reg = mod.cross_validate(kNN_test, xDep, yDep, cv=cv)
+print(lin_cval['test_score'])
+print(kNN_cval['test_score']) """
 
 sns.set()
-plt.scatter(xDep, yDep, color='red', label='Data points')
-plt.plot(xDep, lin_regDep.predict(xDep), color='blue', label='Linear regression')
-plt.plot(sorted(xDep), kNN_regDep.predict(sorted(xDep)), color='green', label='k-neighbors (k={})'.format(k))
+plt.scatter(xTrain, yTrain, color='red', label='Data points (training)')
+plt.plot(xTrain, lin_regDep.predict(xTrain), color='blue', label='Linear regression')
+plt.plot(sorted(xTrain), kNN_regDep.predict(sorted(xTrain)), color='green', label='k-neighbors (k={})'.format(k))
 plt.xlabel("Simplicity - Measures subject's need to see the world in black and white")
 plt.ylabel("Depression (Beck)")
 plt.title("Regression on simplicity and depression")
@@ -50,4 +60,14 @@ plt.legend()
 plt.savefig("Figures/depression.png")
 plt.show()
 
+
+plt.scatter(xTest, yTest, color='red', label='Data points (test)')
+plt.plot(xTest, lin_regDep.predict(xTest), color='blue', label='Linear regression')
+plt.plot(sorted(xTest), kNN_regDep.predict(sorted(xTest)), color='green', label='k-neighbors (k={})'.format(k))
+plt.xlabel("Simplicity - Measures subject's need to see the world in black and white")
+plt.ylabel("Depression (Beck)")
+plt.title("Regression on simplicity and depression")
+plt.legend()
+plt.savefig("Figures/depression.png")
+plt.show()
 
