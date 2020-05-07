c1f41121fa6dfd1ead7fbc6197b3152dd0b17084
AdamWirehed <adam.wirehed@gmail.com>
2020-05-07 11:17:48 +0200

    Added new and better figures. Fixed the bug where the correct group labels got shuffled because F_groups was inherit from groups. Remember that

--- BigData/project3.py
+++ BigData/project3.py
@@ -20,21 +20,26 @@ gSize = round(p/nGroups)
 beta = np.random.normal(loc=0, scale=1, size=p)
 
 groups = np.ones(p)
+F_groups = np.ones(p)
 start = 0
 
 for ix in range(0, nGroups):
     groups[start:start+gSize] = ix
+    F_groups[start:start+gSize] = ix
     start += gSize
 
 # Uncomment to give feature param. wrong groups
-F_groups = np.random.randint(0, nGroups, p)
+np.random.shuffle(F_groups)
+
+print(groups)
+print(F_groups)
 
 # Generate data
 X = np.ndarray((n,p), dtype=float)
 cov_int = [0.1, 0.7, 0.2, 0.4, 0.3]     # Adds correlation between features within TRUE group
 
 # Uncomment to have 0 correlation between features
-cov_int = np.zeros(p)
+#cov_int = np.zeros(p)
 
 start = 0
 for ig in range(0, nGroups):
@@ -77,6 +82,43 @@ print("Number of zero coefficients: {}".format(nZeros))
 print("Number of choosen variables: {}".format(gl.sparsity_mask_.sum()))
 print(conf_m)
 
+group_bool = np.ndarray(shape=(3, nGroups), dtype=int)
+group_bool[0, :] = [0, 1, 2, 3, 4]
+group_bool[1, :] = [nZeros/2, nZeros/2, 0, 0, 0]
+
+Fgroup_bool = np.ndarray(shape=(3, nGroups), dtype=int)
+Fgroup_bool[0, :] = [0, 1, 2, 3, 4]
+
+
+# print(beta_hat[np.where(groups == 0)])
+# print(beta_hat[np.where(groups == 1)])
+# print(beta_hat[np.where(groups == 2)])
+# print(beta_hat[np.where(groups == 3)])
+# print(beta_hat[np.where(groups == 4)])
+
+# print(beta_hat[np.where(F_groups == 0)])
+# print(beta_hat[np.where(F_groups == 1)])
+# print(beta_hat[np.where(F_groups == 2)])
+# print(beta_hat[np.where(F_groups == 3)])
+# print(beta_hat[np.where(F_groups == 4)])
+
+for ig in range(0, nGroups):
+    ix_g = np.where(groups == ig)
+    ix_fg = np.where(F_groups == ig)
+    sum_0_fg = sum(F_groups[:nZeros] == ig)   # In which false group are the true 0 at?
+    nr_zeros = sum(gl.sparsity_mask_[ix_g] == False)
+    nr_zeros_F = sum(gl.sparsity_mask_[ix_fg] == False)
+    group_bool[2, ig] = nr_zeros
+    Fgroup_bool[2, ig] = nr_zeros_F
+    Fgroup_bool[1, ig] = sum_0_fg
+
+print(gl.sparsity_mask_)
+
+print()
+print(group_bool)
+print()
+print(Fgroup_bool)
+
 sns.set()
 
 plt.figure()
@@ -94,17 +136,9 @@ plt.title("Group Lasso estimation for p={}, n={}, nZeros={}".format(p, n, nZeros
 plt.ylabel("Learned coefficients")
 plt.xlabel("True coefficients")
 
-group_bool = np.ndarray(shape=(3, nGroups), dtype=int)
-group_bool[0, :] = [0, 1, 2, 3, 4]
-group_bool[1, :] = [nZeros/2, nZeros/2, 0, 0, 0]
-
-for ig in range(0, nGroups):
-    ix_g = np.where(groups == ig)
-    nr_zeros = sum(gl.sparsity_mask_[ix_g] == False)
-    group_bool[2, ig] = nr_zeros
-
-print()
-print(group_bool)
+plt.figure()
+sns.heatmap(conf_m, annot=True, fmt='d', yticklabels=["True 0", "True non-0"], xticklabels=["Est. 0", "Est. non-0"])
+plt.title("Confusion Matrix")
 
 plt.figure()
 sns.heatmap(group_bool[1:, :], annot=True, fmt='d', yticklabels=False)
@@ -112,10 +146,13 @@ plt.title("'Confusion' - (not really) matrix")
 plt.xlabel("Group Index")
 plt.ylabel("Est. # zeros          |         True # zeros")   
 
+plt.figure()
+sns.heatmap(Fgroup_bool[1:, :], annot=True, fmt='d', yticklabels=False)
+plt.title("'Confusion' - (not really) matrix")
+plt.xlabel("False Group Index")
+plt.ylabel("Est. # zeros          |         True # zeros")   
 
 plt.show()
-
-
 
 
 
